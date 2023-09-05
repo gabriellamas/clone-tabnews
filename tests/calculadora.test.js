7c6ccaf2b50cb23f4c5d81de4d9269c24f84c0c9
Gabriel Lamas <gabriellamas@macecoy92xh595vf.local>
2023-09-05 17:05:28 -0300

    true test exercice

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,11 @@
-test("espero que 1 seja 1", () => {
-  expect(1).toBe(1);
+const calculadora = require("../models/calculadora");
+
+test("somar 2 + 2 = 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+});
+
+test("salada + 2 = 4", () => {
+  const resultado = calculadora.somar("salada", 2);
+  expect(resultado).toBe("Error");
 });
