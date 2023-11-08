547271953aacbf932f9c7fbd1d411b7442c776c3
Gabriel Rosa <gabrielldarts@gmail.com>
2023-11-08 17:38:05 +0000

    add `database.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function Status(request, response) {
+import database from "../../../../infra/database"
+
+async function Status(request, response) {
+  const result = await database.query('SELECT 1+1 as sum;')
+  console.log(result.rows)
   response.status(200).send({ message: "Alguma coisa" });
 }
 
